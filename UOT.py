b039d3fc017dc2d893ef7847e46f9e3ee437370d
drproduck <duckhiem95@gmail.com>
2020-08-05 20:31:50 +0700

    added gradient descent

--- UOT.py
+++ UOT.py
@@ -1,7 +1,7 @@
-import numpy as np
 import torch
 from time import time
 from .distance import batch_eudist_sq
+import pdb
 
 
 # utilities
@@ -21,13 +21,19 @@ def sampling_sinkhorn_uot(r_sp, c_sp, eta=1.0, t1=1.0, t2=1.0, n_iter=100):
     return sinkhorn_uot(C, r, c, eta=eta, t1=t1, t2=t2, n_iter=100)
 
     
-def sampling_sinkhorn_uot(r_sp, c_sp, eta=0.1, t1=1., t2=1., n_iter=100):
+def sampling_sinkhorn_uot(r_sp, c_sp, r=None, c=None, eta=0.1, t1=1., t2=1., method='sinkhorn', n_iter=100, gamma=1e-3):
     C = batch_eudist_sq(r_sp, c_sp)
     nr, nc = r_sp.shape[0], c_sp.shape[0]
-    r = torch.ones((nr, 1), dtype=torch.float32).cuda() / nr
-    c = torch.ones((nc, 1), dtype=torch.float32).cuda() / nc
+    if r is None:
+        r = torch.ones((nr, 1), dtype=torch.float32).cuda() / nr
+    if c is None:
+        c = torch.ones((nc, 1), dtype=torch.float32).cuda() / nc
     
-    return sinkhorn_uot(C, r, c, eta=eta, t1=t1, t2=t2, n_iter=n_iter)
+    if method == 'sinkhorn':
+        return sinkhorn_uot(C, r, c, eta=eta, t1=t1, t2=t2, n_iter=n_iter)
+    elif method == 'gd':
+        return grad_descent_uot(C, r, c, eta=eta, t1=t1, t2=t2, n_iter=n_iter, gamma=gamma)
+
 
 def sinkhorn_uot(C, r, c, eta=0.1, t1=1.0, t2=1.0, n_iter=100):
     """
@@ -42,6 +48,7 @@ def sinkhorn_uot(C, r, c, eta=0.1, t1=1.0, t2=1.0, n_iter=100):
     # initial solution
     u = torch.zeros_like(r)
     v = torch.zeros_like(c)
+    
 
     for i in range(n_iter):
         S = get_S(C, u, v, eta)
@@ -55,3 +62,30 @@ def sinkhorn_uot(C, r, c, eta=0.1, t1=1.0, t2=1.0, n_iter=100):
     S = get_S(C, u, v, eta)
 
     return S
+
+def grad_descent_uot(C, r, c, eta=1.0, t1=1.0, t2=1.0, gamma=1e-3, n_iter=100):
+ 
+    """
+    :arg C: cost matrix shape = [r_dim, c_dim]
+    :arg r: first marginal shape = [r_dim, 1]
+    :arg c: second marginal shape = [c_dim, 1]
+    :arg eta: entropic-regularizer
+    :arg t1: first KL regularizer
+    :arg t2: second Kl regularizer
+    :arg gamma: step size
+    :n_iter: number of Sinkhorn iterations
+    """
+
+    log_r = torch.log(r)
+    log_c = torch.log(c).reshape(1, -1)
+
+    X = torch.rand_like(C)
+
+    for it in range(n_iter):
+        log_sum_r = X.sum(dim=-1, keepdim=True).log() # [r_dim, 1]
+        log_sum_c = X.sum(dim=0, keepdim=True).log() # [1, c_dim]
+        delta = C + t1 * log_sum_r + t2 * log_sum_c - t1 * log_r - t2 * log_c + eta * torch.log(X)
+
+        X = torch.max(1e-10, X - gamma * delta)
+
+    return X
